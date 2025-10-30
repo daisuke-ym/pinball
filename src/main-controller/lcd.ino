8f02d279f3b71d819abaec496616624a8d389731
daisuke <daisuke.ym@gmail.com>
2025-10-30 18:16:06 +0900

    受信したCANメッセージをシリアルに表示しないようにした

--- src/main-controller/lcd.ino
+++ src/main-controller/lcd.ino
@@ -35,25 +35,6 @@ void disp_lcd_can_message(unsigned long rxId, uint8_t len, uint8_t *rxBuf) {
   static uint8_t extraballProgressIndex = 0;
 
   /*
-  // Print CAN ID
-  LCD.setCursor(0, 1);
-  LCD.print("         "); // Clear line
-  sprintf(msg, "%03x", rxId);
-  LCD.setCursor(0, 1);
-  LCD.print(msg);
-  Serial.print(msg);
-  // Print data length
-  sprintf(msg, "(%d)", len);
-  LCD.print(msg);
-  Serial.print(msg);
-  // Print payload
-  for (int i = 0; i < len; i++) {
-    sprintf(msg, "%02x ", rxBuf[i]);
-    LCD.print(msg);
-    Serial.print(msg);
-  }
-	Serial.println();
-  */
   sprintf(msg, "0x%03X ", rxId);
   Serial.print(msg);
   for (int i = 0; i < len; i++) {
@@ -61,6 +42,7 @@ void disp_lcd_can_message(unsigned long rxId, uint8_t len, uint8_t *rxBuf) {
     Serial.print(msg);
   }
 	Serial.println();
+  */
 
   // 各アイテムごとのステータス表示
   switch (rxId) {
